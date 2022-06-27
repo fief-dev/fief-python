e9aa7918ef290dc01acd9fba12efcd507862930b
François Voron <fvoron@gmail.com>
2022-06-27 13:39:52 +0200

    Add PKCE helper functions

---
+++ fief_client/pkce.py
@@ -0,0 +1,37 @@
+import base64
+import hashlib
+import secrets
+import sys
+
+if sys.version_info < (3, 8):
+    from typing_extensions import Literal  # pragma: no cover
+else:
+    from typing import Literal  # pragma: no cover
+
+
+def get_code_verifier() -> str:
+    """
+    Generate a code verifier suitable for PKCE.
+    """
+    return secrets.token_urlsafe(96)
+
+
+Method = Literal["plain", "S256"]
+
+
+def get_code_challenge(code: str, method: Method = "S256") -> str:
+    """
+    Generate the PKCE code challenge for the given code and method.
+
+    :param code: The code to generate the challenge for.
+    :param method: The method to use for generating the challenge. Either "plain" or "S256".
+    """
+    if method == "plain":
+        return code
+
+    if method == "S256":
+        hasher = hashlib.sha256()
+        hasher.update(code.encode("ascii"))
+        digest = hasher.digest()
+        b64_digest = base64.urlsafe_b64encode(digest).decode("ascii")
+        return b64_digest[:-1]  # Remove the padding "=" at the end
---
+++ tests/test_pkce.py
@@ -0,0 +1,26 @@
+import pytest
+
+from fief_client.pkce import Method, get_code_challenge, get_code_verifier
+
+
+def test_get_code_verifier():
+    code = get_code_verifier()
+    assert type(code) == str
+    assert len(code) == 128
+
+
+@pytest.mark.parametrize(
+    "code,method",
+    [
+        ("A" * 128, "plain"),
+        ("A" * 128, "S256"),
+    ],
+)
+def test_code_challenge(code: str, method: Method):
+    challenge = get_code_challenge(code, method)
+    assert type(challenge) == str
+
+    if method == "plain":
+        assert challenge == code
+    elif method == "S256":
+        assert len(challenge) == 43
